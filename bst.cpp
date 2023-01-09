a39cd38746b5195132f69d1ea21c0e1a05b74edd
Mohamed KARIM <karimkar999@gmail.com>
2023-01-09 11:58:30 +0100

    add a member function to check if the tree is balanced or not

--- bst.cpp
+++ bst.cpp
@@ -6,7 +6,7 @@
 /*   By: mkarim <mkarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 10:20:05 by mkarim            #+#    #+#             */
-/*   Updated: 2023/01/08 21:57:52 by mkarim           ###   ########.fr       */
+/*   Updated: 2023/01/09 11:57:26 by mkarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ class   BST {
             int     data;
             Node*   left;
             Node*   right;
+            int     height;
         };
         Node*       root;
 
@@ -36,6 +37,7 @@ class   BST {
             newNode->data = value;
             newNode->left = NULL;
             newNode->right = NULL;
+            newNode->height = 0;
             return newNode;
         }
 
@@ -49,9 +51,15 @@ class   BST {
             if (node == NULL)
                 return newNode(data);
             if (node->data > data)
+            {
                 node->left = insert(node->left, data);
+                node->height = node->left->height + 1;
+            }
             else if (node->data < data)
+            {
                 node->right = insert(node->right, data);
+                node->height = node->right->height + 1;
+            }
             return node;
         }
         
@@ -69,7 +77,7 @@ class   BST {
             for (int i = 0; i < depth; i++) {
                 std::cout << "   ";
             }
-            std::cout << root->data << std::endl;
+            std::cout << root->data << " - " << root->height << std::endl;
 
             printTree(root->left, depth+1);
         }
@@ -118,12 +126,39 @@ class   BST {
             print_postorder(root->right);
             std::cout << root->data << " ";
         }
+
+        bool    balanced()
+        {
+            return balanced(root);
+        }
+
+        bool    balanced(Node* root)
+        {
+            if (root == NULL || (root->left == NULL && root->right == NULL))
+                return true;
+            if (root->left && root->right == NULL)
+            {
+                if (root->left->height > 1) return false;
+                return (balanced(root->left));
+            }
+            if (root->right && root->left == NULL)
+            {
+                if (root->right->height > 1) return false;
+                return (balanced(root->right));
+            }
+            return (abs(root->right->height - root->left->height) <= 1);
+        }
 };
 
 int main()
 {
     BST bst;
 
+    // bst.insert(1);
+    // bst.insert(10);
+    // bst.insert(100);
+    // bst.insert(1000);
+    
     bst.insert(20);
     bst.insert(10);
     bst.insert(40);
@@ -131,10 +166,13 @@ int main()
     bst.insert(15);
     bst.insert(50);
     bst.insert(30);
+    bst.insert(100);
+    bst.insert(10000);
 
     bst.printTree();
-    bst.print_inorder();
-    bst.print_preorder();
-    bst.print_postorder();
+    std::cout << (bst.balanced() == true ? "Yes, it's balanced" : "No, it's not") << std::endl;
+    // bst.print_inorder();
+    // bst.print_preorder();
+    // bst.print_postorder();
     return 0;
 }
