e95255e48e0ed7e4c26741007c4c2708371ecf5f
Mohamed KARIM <karimkar999@gmail.com>
2023-01-15 09:16:26 +0100

    add remove member function to delete a node from the tree

--- bst.cpp
+++ bst.cpp
@@ -6,11 +6,12 @@
 /*   By: mkarim <mkarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 10:20:05 by mkarim            #+#    #+#             */
-/*   Updated: 2023/01/09 12:06:29 by mkarim           ###   ########.fr       */
+/*   Updated: 2023/01/15 09:15:11 by mkarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <algorithm>
 #include <queue>
 
 class   BST {
@@ -75,9 +76,9 @@ class   BST {
             printTree(root->right, depth+1);
 
             for (int i = 0; i < depth; i++) {
-                std::cout << "   ";
+                std::cout << "        ";
             }
-            std::cout << root->data << " - " << root->height << std::endl;
+            std::cout << root->data << ", h : " << root->height << std::endl << std::endl;
 
             printTree(root->left, depth+1);
         }
@@ -164,6 +165,122 @@ class   BST {
                 return found(root->right, val);
             return true;
         }
+
+        void    remove(int val)
+        {
+            remove(root, val);
+        }
+
+//         void    remove(Node*& root, int val)
+//         {
+//             if (root == NULL)
+//                 return ;
+//             if (root->data == val)
+//             {
+//                 if (root->left == NULL && root->right == NULL)
+//                 {
+//                     delete root;
+//                     root = nullptr;
+//                 }
+//                 else if (root->left == NULL && root->right)
+//                 {
+//                     root->data = root->right->data;
+//                     root->left = root->right->left;
+//                     root->right = root->right->right;
+//                     root->height = root->right->height;
+//                     delete root->right;
+//                     root->right = nullptr;
+//                 }
+//                 else if (root->left && root->right == NULL)
+//                 {
+//                     root->data = root->left->data;
+//                     root->right = root->left->right;
+//                     root->left = root->left->left;
+//                     root->height = root->height;
+//                     delete root->left;
+//                     root->left = nullptr;
+//                 }
+//                 else
+//                 {
+//                     Node* before = root->right;
+//                     Node* tmp = root->right;
+//                     while (tmp)
+//                     {
+//                         before = tmp;
+//                         tmp = tmp->left;
+//                     }
+//                     root->data = before->data;
+//                     delete before;
+//                     before = nullptr;
+//                 }
+//             }
+//             else if (root->data > val)
+//                 remove(root->left, val);
+//             else
+//                 remove(root->right, val);
+//         }
+// };
+
+        Node* findMin(Node* root)
+        {
+            while (root->left != NULL)
+                root = root->left;
+            return root;
+        }
+        
+        int     max_of_height(Node*& n1, Node*& n2)
+        {
+            if (!n1 && !n2) return 0;
+            if (!n1) return n1->height;
+            if (!n2) return n2->height;
+            return std::max(n1->height, n2->height);
+        }
+        void remove(Node*& root, int val)
+        {
+            if (root == NULL) return;
+
+            if (val < root->data)
+                remove(root->left, val);
+            else if (val > root->data)
+                remove(root->right, val);
+            else 
+            {
+                if (root->left == NULL && root->right == NULL)
+                {
+                    delete root;
+                    root = nullptr;
+                }
+                else if (root->left == NULL)
+                {
+                    Node* temp = root;
+                    root = root->right;
+                    delete temp;
+                }
+                else if (root->right == NULL)
+                {
+                    Node* temp = root;
+                    root = root->left;
+                    delete temp;
+                }
+                else
+                {
+                    Node* temp = findMin(root->right);
+                    root->data = temp->data;
+                    remove(root->right, temp->data);
+                }
+            }
+            if (root != NULL)
+            {
+                if (root->left == NULL && root->right == NULL)
+                    root->height = 0;
+                else if (root->left == NULL)
+                    root->height = root->right->height + 1;
+                else if (root->right == NULL)
+                    root->height = root->left->height + 1;
+                else
+                    root->height = std::max(root->left->height, root->right->height) + 1;
+            }
+        }
 };
 
 int main()
@@ -185,10 +302,16 @@ int main()
     bst.insert(100);
     bst.insert(10000);
 
+    bst.remove(20);
+    bst.remove(100);
+    bst.remove(10000);
+    bst.remove(50);
+    bst.remove(40);
+
     bst.printTree();
 
-    std::cout << (bst.balanced() == true ? "Yes, it's balanced" : "No, it's not") << std::endl;
-    std::cout << (bst.found(370) == true ? "Yes, we found it" : "No, it's not exist") << std::endl;
+    // std::cout << (bst.balanced() == true ? "Yes, it's balanced" : "No, it's not") << std::endl;
+    // std::cout << (bst.found(370) == true ? "Yes, we found it" : "No, it's not exist") << std::endl;
     // bst.print_inorder();
     // bst.print_preorder();
     // bst.print_postorder();
